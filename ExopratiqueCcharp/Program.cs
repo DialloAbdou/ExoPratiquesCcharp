57c538af27f6c4c66ef70fff92d698f38fb8635b
= <=dialloabdou@hotmail.com>
2023-10-04 12:39:29 +0200

    modif de caractere dans la fonction et declaration variable

---
+++ ExopratiqueCcharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExopratiqueCcharp.session2;
+
+Console.WriteLine(" Traitement des différents Exercices");
+
+var t = Exercice1.Main();
+
+Console.WriteLine($"name: {t.Item1}, age : {t.Item2} , weight : {t.Item3}, isHuman: {t.Item4}");
+
+Console.WriteLine("===DisplayCompute===");
+Exercice1.ComputeAndDisplay(10,2);
+Exercice1.ComputeAndDisplay(21,21);
+Exercice1.ComputeAndDisplay(99,3);
+
+
+
---
+++ ExopratiqueCcharp/session2/Exercice1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExopratiqueCcharp.session2
+{
+    public class Exercice1
+    {
+        /// <summary>
+        ///  Utilisation de Tuple
+        /// </summary>
+        /// <returns></returns>
+        public static Tuple<string, int, float,bool,string[]> Main()
+        {
+            // TODO
+            var name = "John";
+            var age = 25;
+            var weight =78.4;
+            var isHuman = true;
+            var friends =  new string[]{"Paul", "Sam","David"};
+            
+            return new Tuple<string, int, float,bool,string[]>
+                (name, age, (float) weight, isHuman, friends);
+        }
+
+        /// <summary>
+        /// le caractere à modifier
+        /// pour une fonction qui fait du modulo
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+         public  static void ComputeAndDisplay(int a, int b)
+         {
+           var res = a % b;
+           Console.WriteLine(res);
+         }
+    }
+}
